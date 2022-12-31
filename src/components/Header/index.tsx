08ad17b4f5328ee7427fd8838e31aeb1ac0cdf15
Neka <nekoanchor@yahoo.fr>
2022-12-31 19:01:15 +0100

    feat: add header without responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,42 @@
 // Styles
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
-const Header = () => {
-    return (
-        <header className={styles.__header}>
-            <p>Header</p>
-        </header>
-    )
+// Assets
+import logo from "../../assets/images/logo.png";
+
+interface HeaderProps {
+    links: {
+        id: number;
+        name: string;
+        url: string;
+    }[];
 }
+
+const Header = ({ links }: HeaderProps) => {
+
+    type Link = {
+        id: number;
+        name: string;
+        url: string;
+    }
+
+  return (
+    <header className={styles.__header}>
+      <div className={styles.__brand}>
+        <img className={styles.__logo} src={logo} alt="logo umbrella" />
+        <span className={styles.__name}>Umbrella</span>
+      </div>
+      <ul>
+        {links.map(({ id, name, url }: Link) => {
+          return (
+            <li key={`link-${name}-${id}`}>
+              <a href={url}>{name}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,22 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 const Home = () => {
-    return(
-        <div>
-            <Header />
-            <p>Content</p>
-            <Footer />
-        </div>
-    )
+
+  // Data Header
+  const links = [
+    { id: 1, name: "Lorem ipsum", url: "#" },
+    { id: 2, name: "Consectetur", url: "#" },
+    { id: 3, name: "Adipiscing", url: "#" },
+    { id: 4, name: "Eiusmod", url: "#" },
+  ];
+
+  return (
+    <div>
+      <Header links={links} />
+      <p>Content</p>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
