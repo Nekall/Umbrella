8f7db58f5f1779a80baa9f10d5723fb98090b7f7
Neka <nekoanchor@yahoo.fr>
2023-01-05 23:50:42 +0100

    feat: add pwd gen cmp

---
+++ src/components/PasswordGenerator/index.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+// Styles
+import styles from "./styles.module.scss";
+
+const PasswordGenerator = () => {
+  const [password, setPassword] = useState<string>("Simple-Mémo");
+
+  const [randomString, setRandomString] = useState({
+    rdmstr1: Math.random().toString(36).substring(2, 15),
+    rdmstr2: Math.random().toString(36).substring(2, 15),
+    rdmstr3: Math.random().toString(36).substring(2, 15),
+    rdmstr4: Math.random().toString(36).substring(2, 15),
+    rdmstr5: Math.random().toString(36).substring(2, 15),
+    rdmstr6: Math.random().toString(36).substring(2, 15),
+    rdmstr7: Math.random().toString(36).substring(2, 15),
+    rdmstr8: Math.random().toString(36).substring(2, 15),
+  });
+
+  const updateRandomString = () => {
+    setRandomString({
+      rdmstr1: Math.random().toString(36).substring(2, 15),
+      rdmstr2: Math.random().toString(36).substring(2, 15),
+      rdmstr3: Math.random().toString(36).substring(2, 15),
+      rdmstr4: Math.random().toString(36).substring(2, 15),
+      rdmstr5: Math.random().toString(36).substring(2, 15),
+      rdmstr6: Math.random().toString(36).substring(2, 15),
+      rdmstr7: Math.random().toString(36).substring(2, 15),
+      rdmstr8: Math.random().toString(36).substring(2, 15),
+    });
+  };
+
+  return (
+    <div className={styles.__password_generator}>
+      <h2 className={styles.__title}>
+        Generer des mots de passes efficaces & sécurisés.
+      </h2>
+      <p className={styles.__description}>
+        <span>Logique :</span> Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </p>
+      <div className={styles.__table}>
+        <table>
+          <tbody>
+            <tr>
+              <td>{randomString.rdmstr1}</td>
+              <td className={styles.__middle_column}>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </td>
+              <td>{randomString.rdmstr2}</td>
+            </tr>
+            <tr>
+              <td>{randomString.rdmstr3}</td>
+              <td className={styles.__middle_column}>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </td>
+              <td>{randomString.rdmstr4}</td>
+            </tr>
+            <tr>
+              <td>{randomString.rdmstr5}</td>
+              <td className={styles.__middle_column}>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </td>
+              <td>{randomString.rdmstr6}</td>
+            </tr>
+            <tr>
+              <td>{randomString.rdmstr7}</td>
+              <td className={styles.__middle_column}>
+                <input
+                  type="text"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </td>
+              <td>{randomString.rdmstr8}</td>
+            </tr>
+            <tr>
+              <td className={styles.__last_row}></td>
+              <td className={`${styles.__last_row} ${styles.__middle_column}`}>
+                <button onClick={() => updateRandomString()}>
+                  Generer
+                  <br />
+                  des mots de passes
+                </button>
+              </td>
+              <td className={styles.__last_row}></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default PasswordGenerator;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,14 @@
 // Components
+import Hero from "../../components/Hero";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import ThreeCards from "../../components/ThreeCards";
-import Hero from "../../components/Hero";
+import PasswordGenerator from "../../components/PasswordGenerator";
 
 // Styles
 import styles from "../../styles/pages/home.module.scss";
 
 const Home = () => {
-
   // Data Header
   const links = [
     { id: 1, name: "Lorem ipsum", url: "#" },
@@ -22,6 +22,7 @@ const Home = () => {
       <Header links={links} />
       <Hero />
       <ThreeCards />
+      <PasswordGenerator />
       <p className={styles.__fake_content}>Content</p>
       <Footer />
     </div>
