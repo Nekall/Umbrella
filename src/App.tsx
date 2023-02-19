06c43a5221029beb7433bdbbdd9aae21c903b284
Neka <nekoanchor@yahoo.fr>
2023-02-19 21:29:13 +0100

    feat: add console log

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,25 @@ import Home from "./pages/Home";
 const { REACT_APP_MAINTENANCE } = process.env;
 
 const App = () => {
+  console.clear();
+  console.log(`%c
+1 1 001 00010 1 00 10 1
+ 0 11 1 0011 0 10 1 0
+1 101 01010 1 01 10 1 0
+  0 110 1__.|.__ 0 0 11
+0  1.-"'..':'..'"-. 0 1
+  .' .' .  :  . '. '.
+1/ .   .   :   .   . \\0
+/_ _._ _.._:_.._ _._ _\\
+  '   '    |    '   '
+           |
+           | Umbrella
+           |
+           |  Sécurité
+           |  & Prévention
+         '=' 
+
+`, 'color: #808080')
   const maintenance = REACT_APP_MAINTENANCE ? JSON.parse(REACT_APP_MAINTENANCE) : false;
   return maintenance ? <Maintenance /> : <Home />;
 };
